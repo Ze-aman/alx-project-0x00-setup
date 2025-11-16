9d0fa075ef4549272b154af5e936313b9a4fa0af
Ze-aman <aman09dida@gmail.com>
2025-11-16 22:34:10 +0300

    Update index.ts

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -7,4 +7,5 @@ export interface ButtonProps {
   size?: "small" | "medium" | "large"; // Optional size
   shape?: "rounded-sm" | "rounded-md" | "rounded-full"; // Optional shape
   className?: string;  // Optional additional Tailwind styles
+  styles: string
 }
